0098368caf39319c17eeb6dc46a1547119b7c249
R3FL3X <70488503+ReflexTheLegend@users.noreply.github.com>
2022-06-26 14:56:24 +0300

    Add files via upload

--- windowsfetch.py
+++ windowsfetch.py
@@ -14,6 +14,7 @@ try:
     from uptime import uptime
     import pyautogui
     import requests
+    import datetime
 except ImportError:
     os.system('cls')
     try:
@@ -33,15 +34,38 @@ except ImportError:
         os.system('cls')
         print('Installation started...')
         os.system('pip install py-cpuinfo GPUtil psutil colorama uptime pyautogui requests')
-        print('Installation finished, please re-launch the program!')
+        print('Almost ready...')
         time.sleep(5)
         exit()
     except:
-        print('Ready! Re-launch the program!')
+        print('Done! Re-launch the program!')
         time.sleep(3)
         exit()
 
 colorama.init()
+
+def bar(var):
+    if var >= 0 and var <= 10:
+        loadbar = f'[ {colorama.Fore.RED}#{colorama.Fore.RESET}--------- ]'
+    elif var > 10 and var <= 20:
+        loadbar = f'[ {colorama.Fore.RED}##{colorama.Fore.RESET}-------- ]'
+    elif var > 20 and var <= 30:
+        laodbar = f'[ {colorama.Fore.YELLOW}###{colorama.Fore.RESET}------- ]'
+    elif var > 30 and var <= 40:
+        loadbar = f'[ {colorama.Fore.YELLOW}####{colorama.Fore.RESET}------ ]'
+    elif var > 40 and var <= 50:
+        loadbar = f'[ {colorama.Fore.GREEN}#####{colorama.Fore.RESET}----- ]'
+    elif var > 50 and var <= 60:
+        loadbar = f'[ {colorama.Fore.GREEN}######{colorama.Fore.RESET}---- ]'
+    elif var > 60 and var <= 70:
+        loadbar = f'[ {colorama.Fore.GREEN}#######{colorama.Fore.RESET}--- ]'
+    elif var > 70 and var <= 80:
+        loadbar = f'[ {colorama.Fore.GREEN}########{colorama.Fore.RESET}-- ]'
+    elif var > 80 and var <= 90:
+        loadbar = f'[ {colorama.Fore.GREEN}#########{colorama.Fore.RESET}- ]'
+    elif var > 90 and var <= 100:
+        loadbar = f'[ {colorama.Fore.GREEN}##########{colorama.Fore.RESET} ]'
+    return loadbar
 
 class colors:
     HEADER = '\033[95m'
@@ -53,6 +77,18 @@ class colors:
     ENDC = '\033[0m'
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
+
+os.system('cls')
+print(f'{colors.FAIL}Fetching PC infos please wait.', end='')
+
+def convert(seconds):
+    seconds = seconds % (24 * 3600)
+    hour = seconds // 3600
+    seconds %= 3600
+    minutes = seconds // 60
+    seconds %= 60
+      
+    return "%d:%02d:%02d" % (hour, minutes, seconds)
 
 ## ---GENERAL VARIABLES--- ##
 
@@ -73,14 +109,25 @@ except:
     pc_username, pc_name, arch = 'N/A'
 
 #  GPU INFO (ONLY NVIDIA)  #
-gpus = GPUtil.getGPUs()
+try:
+    gpus = GPUtil.getGPUs()
+except:
+    gpus = []
 if len(gpus)!=0:
     for gpu in gpus:
         gpu_id = gpu.name
         gpu_mem = int(gpu.memoryTotal/1024)
+        gpu_used_memory = gpu.memoryUsed
+        gpu_total_memory = gpu.memoryTotal
+        gpu_temperature = gpu.temperature
+        gpuper = round(gpu_used_memory/gpu_total_memory*100, 2)
 else:
     gpu_id = 'N/A'
     gpu_mem = '0'
+    gpuper = '0'
+    gpu_temperature = '0'
+
+print(f'{colors.FAIL}.', end='')
 
 #  OS INFO  #
 opsys = platform.system()
@@ -155,16 +202,49 @@ disks = len(psutil.disk_partitions())
 #  PYTHON VERSION  #
 pyver = platform.python_version()
 
+print(f'{colors.FAIL}.', end='')
+
 #  IP ADDRESS  #
 timeout = 5
 try:
-    api = requests.get('https://ipinfo.io/json', timeout=timeout)
+    api = requests.get('https://ipapi.co/json', timeout=timeout)
     json = api.json()
     myip = json['ip']
+    countryname = json['country_name']
+    countrynamecode = json['country_code']
+    proxapi = requests.get(f'https://proxycheck.io/v2/{myip}?vpn=1&asn=1', timeout=timeout)
+    json2 = proxapi.json()
+    isproxy = json2[f'{myip}']['proxy']
+    if isproxy == 'no':
+        isproxy = 'Clean IP'
+    elif isproxy == 'yes':
+        isproxy = 'Likely Proxy/VPN'
+    else:
+        isproxy = 'N/A'
 except(requests.ConnectionError, requests.Timeout) as exception:
     myip = 'N/A'
+    countryname = '?'
+    countrynamecode = '?'
+    isproxy = 'N/A'
+
+#  BATTERY INFO  #
+try:
+    battery = psutil.sensors_battery()
+    batterpercent = battery[0]
+    charging = battery[2]
+    barforpercent = bar(batterpercent)
+    if charging == True:
+        charging = 'Charging'
+    else:
+        charging = 'Discharging'
+except:
+    battery = 'NO BATTERY'
+    batterpercent = '-'
+    charging = '-'
+    barforpercent = '[----------]'
 
 def w10nf():
+    os.system('cls')
     dashes = len(pc_name+pc_username)
     dashes = dashes+1
     dashes1 = ('-' * dashes)
@@ -179,15 +259,16 @@ llllllllllllllF lllllllllllllllllll         {colorama.Fore.GREEN}Apps{colorama.F
 **************` *******************         {colorama.Fore.GREEN}Terminal{colorama.Fore.WHITE}: {term}{colors.BLUE}
 ggggggggggggggr ggwwwwwwwwwwwwwwwww         {colorama.Fore.GREEN}Resolution{colorama.Fore.WHITE}: {width}x{height}{colors.BLUE}
 llllllllllllllF lllllllllllllllllll         {colorama.Fore.GREEN}CPU{colorama.Fore.WHITE}: {cpuname} ({cpucores}){colors.BLUE}
-llllllllllllllF lllllllllllllllllll         {colorama.Fore.GREEN}GPU{colorama.Fore.WHITE}: {gpu_id} {gpu_mem}GB{colors.BLUE}
+llllllllllllllF lllllllllllllllllll         {colorama.Fore.GREEN}GPU{colorama.Fore.WHITE}: {gpu_id} {gpu_mem}GB ({gpuper}%) - ({gpu_temperature}°C){colors.BLUE}
 llllllllllllllF lllllllllllllllllll         {colorama.Fore.GREEN}Memory{colorama.Fore.WHITE}: {usedram}GB / {totalram}GB ({usedrampercent}%){colors.BLUE}
 llllllllllllllF lllllllllllllllllll         {colorama.Fore.GREEN}Disks{colorama.Fore.WHITE}: {disks}{colors.BLUE}
 &&$lllllllllllF lllllllllllllllllll         {colorama.Fore.GREEN}Python{colorama.Fore.WHITE}: {pyver}{colors.BLUE}
-         '"***  &&$llllllllllllllll         {colorama.Fore.GREEN}IP{colorama.Fore.WHITE}: {myip}{colors.BLUE}
-                         '"****&&&l
+         '"***  &&$llllllllllllllll         {colorama.Fore.GREEN}IP{colorama.Fore.WHITE}: {myip}, {countryname} ({countrynamecode}), {isproxy}{colors.BLUE}
+                         '"****&&&l         {colorama.Fore.GREEN}Battery{colorama.Fore.WHITE}: {barforpercent} {batterpercent}%, {charging}{colors.BLUE}
 ''')
 
 def w11nf():
+    os.system('cls')
     dashes = len(pc_name+pc_username)
     dashes = dashes+1
     dashes1 = ('-' * dashes)
@@ -202,14 +283,16 @@ def w11nf():
                                             {colorama.Fore.GREEN}Terminal{colorama.Fore.WHITE}: {term}{colors.CYAN}
 ################   ################         {colorama.Fore.GREEN}Resolution{colorama.Fore.WHITE}: {width}x{height}{colors.CYAN}
 ################   ################         {colorama.Fore.GREEN}CPU{colorama.Fore.WHITE}: {cpuname} ({cpucores}){colors.CYAN}
-################   ################         {colorama.Fore.GREEN}GPU{colorama.Fore.WHITE}: {gpu_id} {gpu_mem}GB{colors.CYAN}
+################   ################         {colorama.Fore.GREEN}GPU{colorama.Fore.WHITE}: {gpu_id} {gpu_mem}GB ({gpuper}%) - ({gpu_temperature}°C){colors.CYAN}
 ################   ################         {colorama.Fore.GREEN}Memory{colorama.Fore.WHITE}: {usedram}GB / {totalram}GB ({usedrampercent}%){colors.CYAN}
 ################   ################         {colorama.Fore.GREEN}Disks{colorama.Fore.WHITE}: {disks}{colors.CYAN}
 ################   ################         {colorama.Fore.GREEN}Python{colorama.Fore.WHITE}: {pyver}{colors.CYAN}
-################   ################         {colorama.Fore.GREEN}IP{colorama.Fore.WHITE}: {myip}{colors.CYAN}
+################   ################         {colorama.Fore.GREEN}IP{colorama.Fore.WHITE}: {myip}, {countryname} ({countrynamecode}), {isproxy}{colors.CYAN}
+                                            {colorama.Fore.GREEN}Battery{colorama.Fore.WHITE}: {barforpercent} {batterpercent}%, {charging}{colors.CYAN}
 ''')
 
 def w7nf():
+    os.system('cls')
     dashes = len(pc_name+pc_username)
     dashes = dashes+1
     dashes1 = ('-' * dashes)
@@ -224,15 +307,16 @@ def w7nf():
     {colors.BLUE},.=::::!t=., ` {colors.GREEN}@EEEEEEtttz33QF     {colorama.Fore.GREEN}Terminal{colorama.Fore.WHITE}: {term}
    {colors.BLUE};::::::::zt33)   {colors.GREEN}"4EEEtttji3P*      {colorama.Fore.GREEN}Resolution{colorama.Fore.WHITE}: {width}x{height}
   {colors.BLUE}:t::::::::tt33.{colors.YELLOW}:Z3z..  `` ,..g.      {colorama.Fore.GREEN}CPU{colorama.Fore.WHITE}: {cpuname} ({cpucores})
-  {colors.BLUE}i::::::::zt33F {colors.YELLOW}AEEEtttt::::ztF       {colorama.Fore.GREEN}GPU{colorama.Fore.WHITE}: {gpu_id} {gpu_mem}GB
+  {colors.BLUE}i::::::::zt33F {colors.YELLOW}AEEEtttt::::ztF       {colorama.Fore.GREEN}GPU{colorama.Fore.WHITE}: {gpu_id} {gpu_mem}GB ({gpuper}%) - ({gpu_temperature}°C){colors.BLUE}
  {colors.BLUE};:::::::::t33V {colors.YELLOW};EEEttttt::::t3        {colorama.Fore.GREEN}Memory{colorama.Fore.WHITE}: {usedram}GB / {totalram}GB ({usedrampercent}%)
  {colors.BLUE}E::::::::zt33L {colors.YELLOW}@EEEtttt::::z3F        {colorama.Fore.GREEN}Disks{colorama.Fore.WHITE}: {disks}
 {colors.BLUE}(3=*^```"*4E3) {colors.YELLOW};EEEtttt:::::tZ`        {colorama.Fore.GREEN}Python{colorama.Fore.WHITE}: {pyver}
-             ` {colors.YELLOW}:EEEEtttt::::z7         {colorama.Fore.GREEN}IP{colorama.Fore.WHITE}: {myip}
-                 {colors.YELLOW}"VEzjt:;;z>*`
+             {colors.BLUE}` {colors.YELLOW}:EEEEtttt::::z7         {colorama.Fore.GREEN}IP{colorama.Fore.WHITE}: {myip}, {countryname} ({countrynamecode}), {isproxy}{colors.BLUE}
+                 {colors.YELLOW}"VEzjt:;;z>*`         {colorama.Fore.GREEN}Battery{colorama.Fore.WHITE}: {barforpercent} {batterpercent}%, {charging}
 ''')
 
 def w8nf():
+    os.system('cls')
     dashes = len(pc_name+pc_username)
     dashes = dashes+1
     dashes1 = ('-' * dashes)
@@ -247,15 +331,16 @@ llllllllllllllF lllllllllllllllllll         {colorama.Fore.GREEN}Apps{colorama.F
 **************` *******************         {colorama.Fore.GREEN}Terminal{colorama.Fore.WHITE}: {term}{colors.BLUE}
 ggggggggggggggr ggwwwwwwwwwwwwwwwww         {colorama.Fore.GREEN}Resolution{colorama.Fore.WHITE}: {width}x{height}{colors.BLUE}
 llllllllllllllF lllllllllllllllllll         {colorama.Fore.GREEN}CPU{colorama.Fore.WHITE}: {cpuname} ({cpucores}){colors.BLUE}
-llllllllllllllF lllllllllllllllllll         {colorama.Fore.GREEN}GPU{colorama.Fore.WHITE}: {gpu_id} {gpu_mem}GB{colors.BLUE}
+llllllllllllllF lllllllllllllllllll         {colorama.Fore.GREEN}GPU{colorama.Fore.WHITE}: {gpu_id} {gpu_mem}GB ({gpuper}) - ({gpu_temperature}°C){colors.BLUE}
 llllllllllllllF lllllllllllllllllll         {colorama.Fore.GREEN}Memory{colorama.Fore.WHITE}: {usedram}GB / {totalram}GB ({usedrampercent}%){colors.BLUE}
 llllllllllllllF lllllllllllllllllll         {colorama.Fore.GREEN}Disks{colorama.Fore.WHITE}: {disks}{colors.BLUE}
 &&$lllllllllllF lllllllllllllllllll         {colorama.Fore.GREEN}Python{colorama.Fore.WHITE}: {pyver}{colors.BLUE}
-         '"***  &&$llllllllllllllll         {colorama.Fore.GREEN}IP{colorama.Fore.WHITE}: {myip}{colors.BLUE}
-                         '"****&&&l
+         '"***  &&$llllllllllllllll         {colorama.Fore.GREEN}IP{colorama.Fore.WHITE}: {myip}, {countryname} ({countrynamecode}), {isproxy}{colors.BLUE}
+                         '"****&&&l         {colorama.Fore.GREEN}Battery{colorama.Fore.WHITE}: {barforpercent} {batterpercent}%, {charging}{colors.BLUE}
 ''')
 
 def genericwinnf():
+    os.system('cls')
     dashes = len(pc_name+pc_username)
     dashes = dashes+1
     dashes1 = ('-' * dashes)
@@ -270,12 +355,12 @@ def genericwinnf():
     {colors.BLUE},.=::::!t=., ` {colors.GREEN}@EEEEEEtttz33QF     {colorama.Fore.GREEN}Terminal{colorama.Fore.WHITE}: {term}
    {colors.BLUE};::::::::zt33)   {colors.GREEN}"4EEEtttji3P*      {colorama.Fore.GREEN}Resolution{colorama.Fore.WHITE}: {width}x{height}
   {colors.BLUE}:t::::::::tt33.{colors.YELLOW}:Z3z..  `` ,..g.      {colorama.Fore.GREEN}CPU{colorama.Fore.WHITE}: {cpuname} ({cpucores})
-  {colors.BLUE}i::::::::zt33F {colors.YELLOW}AEEEtttt::::ztF       {colorama.Fore.GREEN}GPU{colorama.Fore.WHITE}: {gpu_id} {gpu_mem}GB
+  {colors.BLUE}i::::::::zt33F {colors.YELLOW}AEEEtttt::::ztF       {colorama.Fore.GREEN}GPU{colorama.Fore.WHITE}: {gpu_id} {gpu_mem}GB ({gpuper}%) - ({gpu_temperature}°C)
  {colors.BLUE};:::::::::t33V {colors.YELLOW};EEEttttt::::t3        {colorama.Fore.GREEN}Memory{colorama.Fore.WHITE}: {usedram}GB / {totalram}GB ({usedrampercent}%)
  {colors.BLUE}E::::::::zt33L {colors.YELLOW}@EEEtttt::::z3F        {colorama.Fore.GREEN}Disks{colorama.Fore.WHITE}: {disks}
 {colors.BLUE}(3=*^```"*4E3) {colors.YELLOW};EEEtttt:::::tZ`        {colorama.Fore.GREEN}Python{colorama.Fore.WHITE}: {pyver}
-             ` {colors.YELLOW}:EEEEtttt::::z7         {colorama.Fore.GREEN}IP{colorama.Fore.WHITE}: {myip}
-                 {colors.YELLOW}"VEzjt:;;z>*`
+             {colors.BLUE}` {colors.YELLOW}:EEEEtttt::::z7         {colorama.Fore.GREEN}IP{colorama.Fore.WHITE}: {myip}, {countryname} ({countrynamecode}), {isproxy}
+                 {colors.YELLOW}"VEzjt:;;z>*`         {colorama.Fore.GREEN}Battery{colorama.Fore.WHITE}: {barforpercent} {batterpercent}%, {charging}
 ''')
 
 if release == '10':
